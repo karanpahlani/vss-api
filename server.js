31a43df491241915870a782ed52977f43014da78
Karan Pahlani <pahlani@gmail.com>
2019-03-27 14:21:35 -0500

    updates

--- server.js
+++ server.js
@@ -53,16 +53,16 @@ app.get('/', (req, res) =>{
 
 app.get('/profile/:id', (req, res)=>{
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-            return res.json(user);
+
+    db.select('*').from('passenger').where({userid: id})
+    .then(user => {
+        if(user.length) {
+            res.json(user[0])
+        }else{
+            res.status(400).json('Not Found')
         }
     })
-    if (!found){
-        res.status(400).json('not found');
-    }
+
 })
 
 
