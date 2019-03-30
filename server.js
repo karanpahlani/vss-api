d2cdeea478d2ae4927cd5ff71f12f18f063b2d53
Karan Pahlani <pahlani@gmail.com>
2019-03-30 15:42:45 -0500

    driver passenger login

--- server.js
+++ server.js
@@ -46,24 +46,46 @@ app.get('/profile/:id', (req, res)=>{
 
 
 app.post('/signin', (req,res) =>{
-    db.select('email', 'hash').from('login')
+    db.select('email', 'hash', 'type').from('login')
         .where('email', '=' ,req.body.email)
         .then(data=> {
             const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            console.log(isValid)
-            console.log(data[0])
+            console.log(isValid);
+            console.log(data[0]);
             if(isValid){
-               return db.select('*').from('passenger')
-                    .where('email', '=', req.body.email)
-                    .then(user => {
-                        console.log(user);
-                        var result = {
-                            loggedIn : true,
-                            userData: user[0]
-                        }
-                        res.json(result);
-                    })
-                    .catch(err => res.status(400).json('unable to get user'))
+                if(data[0].type === "passenger") {
+                    console.log("trying to login as passenger")
+                    return db.select('*').from('passenger')
+                        .where('email', '=', req.body.email)
+                        .then(user => {
+                            console.log(user);
+                            var result = {
+                                loggedIn: true,
+                                userData: user[0],
+                                type: 'passenger'
+                            }
+                            res.json(result);
+                        })
+                        .catch(err => res.status(400).json('unable to get user'))
+
+                } else if (data[0].type === "driver"){
+                    console.log("trying to login as driver")
+                    return db.select('*').from('driver')
+                        .where('email', '=', req.body.email)
+                        .then(user => {
+                            console.log(user);
+                            var result = {
+                                loggedIn: true,
+                                userData: user[0],
+                                type: 'driver'
+                            };
+                            res.json(result);
+                        })
+                        .catch(err => res.status(400).json('unable to get user'))
+
+                }
+
+
             } else {
                 var result = {
                     loggedIn: false,
@@ -81,34 +103,81 @@ app.post('/signin', (req,res) =>{
 
 
 app.post('/register', (req,res) =>{
-    const {email, name ,address, password, phone } = req.body;
+    const {email, name ,address, password, phone, type } = req.body;
     const hash = bcrypt.hashSync(password);
 
-        db.transaction(trx => {
-            trx.insert({
-                hash: hash,
-                email: email
-            })
-                .into('login')
-                .returning('email')
-                .then(loginEmail => {
-                    return trx('passenger')
-                        .returning('*')
-                        .insert({
-                            email: loginEmail[0],
-                            name: name,
-                            address: address,
-                            phone: phone,
-                            joined: new Date()
-                        })
-                        .then(user =>{
-                            res.json(user[0]);
-                        })
-                })
-                .then(trx.commit)
-                .catch(trx.rollback)
-        })
-        .catch(err => res.status(400).json('unable to register'))
+
+       if(req.body.type === "passenger") {
+           db.transaction(trx => {
+               trx.insert({
+                   hash: hash,
+                   email: email,
+                   type: type
+               })
+                   .into('login')
+                   .returning('email')
+                   .then(loginEmail => {
+
+                       return trx('passenger')
+                           .returning('*')
+                           .insert({
+                               email: loginEmail[0],
+                               name: name,
+                               address: address,
+                               phone: phone,
+                               joined: new Date()
+                           })
+                           .then(user => {
+
+                               var result = {
+                                   loggedIn: true,
+                                   userData: user[0],
+                                   type: 'passenger'
+                               };
+                               res.json(result);
+                           })
+                   })
+                   .then(trx.commit)
+                   .catch(trx.rollback)
+           })
+               .catch(err => res.status(400).json('unable to register'))
+       }
+       else if(req.body.type === "driver"){
+           db.transaction(trx => {
+               trx.insert({
+                   hash: hash,
+                   email: email,
+                   type: type
+               })
+                   .into('login')
+                   .returning('email')
+                   .then(loginEmail => {
+
+                       return trx('driver')
+                           .returning('*')
+                           .insert({
+                               email: loginEmail[0],
+                               name: name,
+                               address: address,
+                               phone: phone,
+                               joined: new Date()
+                           })
+                           .then(user => {
+
+                               var result = {
+                                   loggedIn: true,
+                                   userData: user[0],
+                                   type: 'driver'
+                               };
+                               res.json(result);
+                           })
+                   })
+                   .then(trx.commit)
+                   .catch(trx.rollback)
+           })
+               .catch(err => res.status(400).json('unable to register'))
+
+       }
 })
 
 
